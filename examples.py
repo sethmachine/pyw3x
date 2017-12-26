e43e626820e04a6d92e2af04b2b51768b4baac49
Seth-David Donald Dworman <sdworman@brandeis.edu>
2017-12-26 14:10:12 -0500

    updated examples with simpler api

--- examples.py
+++ examples.py
@@ -5,8 +5,7 @@ import archive
 infile = 'data/test/Test.w3x'
 mode = 'r' #use 'w' to enable writing
 
-with archive.open_archive(infile, mode) as handle:
-    a = archive.Archive(infile, handle, mode)
+with archive.open_archive(infile, mode) as a:
     listfile = 'list.txt'
     a.extract_list_file(listfile)
     outdir = 'archive-extracted'
