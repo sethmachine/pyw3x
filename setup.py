e47ada5a028c089bdcbf6662d312b51d20764ff1
sethmachine <sdworman@brandeis.edu>
2018-01-13 01:38:31 -0500

    created proper python packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='hivewe',
+      version='0.1',
+      description='Python interface to manipulating Warcraft III map files.',
+      url='https://github.com/sethmachine/hive-world-editor',
+      author='sethmachine',
+      author_email='sethmachine01@gmail.com',
+      license='MIT',
+      packages=['hivewe'],
+      # package_dir={'org.mitre.nlp.mbv':'org/mitre/nlp/mbv'},
+      package_data={'hivewe':['data/storm/*.*', 'data/storm/win-64/*.*']},
+      zip_safe=False)
+
